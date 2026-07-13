708a850ba8ca93e5ff83f3f2622682705ddae4b7
zak969 <mzk16204@gmail.com>
2026-07-13 07:56:20 +0500

    enter

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0) {
+        cout << "Positive Number";
+    }
+
+    return 0;
+}
